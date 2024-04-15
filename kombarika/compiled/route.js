e706216adf6ad692f483628f28c684072937759b
anthonyarimalala <119404637+anthonyarimalala@users.noreply.github.com>
2024-04-15 19:55:52 +0300

    kombarika

---
+++ kombarika/compiled/route.js
@@ -0,0 +1,11 @@
+import { createRouter } from 'vue-router'
+import Personne from "./components/personne/Personne.vue";
+
+
+const routes = {
+	{
+		 path: '/personne', 
+		 component: Personne 
+	 },
+	
+}
